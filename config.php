605fd8ac2e3aadbcfb37fe74a57b233e18e24442
Minh Dieu <minhdieu2003@gmail.com>
2024-04-08 22:25:01 +0700

    Update file kết nối DB

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define("HOST", "localhost");
+define("DB", "luxe_database");
+define("USER", "root");
+define("PASSWORD", "");
+?>
--- db_module.php
+++ db_module.php
@@ -1,11 +1,30 @@
 <?php
-   define("HOST", "localhost");
-   define("DB", "luxe_database");
-   define("USER", "root");
-   define("PASSWORD", "");
+require_once "config.php";
 
+function taoKetNoi(&$link)
+{
     $link = mysqli_connect(HOST,USER,PASSWORD,DB);
     if (mysqli_connect_errno()) {
-        echo "Lỗi kết nối đến máy chủ: " . mysqli_connect_errno();
+        echo "Lỗi kết nối đến máy chủ: " . mysqli_connect_error();
         exit();
     }
+}
+
+function chayTruyVanTraVeDL($link, $q){
+    $result = mysqli_query($link,$q);  
+    return $result;
+}
+
+function chayTruyVanKhongTraVeDL($link, $q){
+    $result = mysqli_query($link,$q);
+    return $result;
+}
+
+function giaiPhongBoNho( $link, $result ){
+    try {
+        mysqli_close($link);
+        mysqli_free_result($result);
+    } catch (TypeError $e) {
+    }
+}
+?>
