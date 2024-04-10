d8d3abadbd4870185bbd98cb66ff58760d55fc9d
Thuhien021003 <nguyenluongthuhienqn1@gmail.com>
2024-04-10 16:24:31 +0700

    Up

--- sanpham.php
+++ sanpham.php
@@ -63,7 +63,7 @@
               </button>
               <div class="top-act-separate"></div>
               <button class="top-act-btn">
-                <img src="./icon/sanpham-cart.svg" alt="" />
+              <a href="./giohang.php?opt=view_sp"><img src="./icon/sanpham-cart.svg" alt="" /></a>
                 <span class="top-act-title"> 03 </span>
               </button>
               <div class="top-act-separate"></div>
@@ -343,6 +343,8 @@
 
                 // Gửi form
                 document.getElementById("form-insert").submit();
+
+                alert("Thêm giỏ hàng thành công");
               }
             </script> 
           </div>
