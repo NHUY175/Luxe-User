8333a427d774f04ce0a34905397d4bdc8830d43b
pekanowf <161617512+pekanowf@users.noreply.github.com>
2024-04-11 15:42:52 +0700

    Update giohang.js
    aaa

--- js/giohang.js
+++ js/giohang.js
@@ -1,71 +1,3 @@
-// Click "-"
-function Decrease() {
-    var count = parseInt(document.getElementById("count").innerHTML);
-    if (count > 0) {
-      count--;
-      document.getElementById("count").innerHTML = count;
-    }
-  }
-  //Click "+"
-  function Increase() {
-    var count = parseInt(document.getElementById("count").innerHTML);
-    count++;
-    document.getElementById("count").innerHTML = count;
-  }
-  // Click "-"
-  function Decrease1() {
-    var count = parseInt(document.getElementById("count1").innerHTML);
-    if (count > 0) {
-      count--;
-      document.getElementById("count1").innerHTML = count;
-    }
-  }
-  //Click "+"
-  function Increase1() {
-    var count = parseInt(document.getElementById("count1").innerHTML);
-    count++;
-    document.getElementById("count1").innerHTML = count;
-  }
-  // Click "-"
-  function Decrease2() {
-    var count = parseInt(document.getElementById("count2").innerHTML);
-    if (count > 0) {
-      count--;
-      document.getElementById("count2").innerHTML = count;
-    }
-  }
-  //Click "+"
-  function Increase2() {
-    var count = parseInt(document.getElementById("count2").innerHTML);
-    count++;
-    document.getElementById("count2").innerHTML = count;
-  }
-  // Click "-"
-  function Decrease3() {
-    var count = parseInt(document.getElementById("count3").innerHTML);
-    if (count > 0) {
-      count--;
-      document.getElementById("count3").innerHTML = count;
-    }
-  }
-  //Click "+"
-  function Increase3() {
-    var count = parseInt(document.getElementById("count3").innerHTML);
-    count++;
-    document.getElementById("count3").innerHTML = count;
-  }
-  
-  function toggleCheckboxes() {
-    var checkboxes = document.querySelectorAll(
-      '.product-detail input[type="checkbox"]'
-    );
-    var masterCheckbox = document.getElementById("checkAll");
-  
-    for (var i = 0; i < checkboxes.length; i++) {
-      checkboxes[i].checked = masterCheckbox.checked;
-    }
-  }
-  
   function darkFunction() {
     const element = document.body;
     element.classList.toggle("dark-mode");
@@ -76,4 +8,3 @@ function Decrease() {
     const imageDelivery = document.querySelector(".delivery img");
   }
 
- 
